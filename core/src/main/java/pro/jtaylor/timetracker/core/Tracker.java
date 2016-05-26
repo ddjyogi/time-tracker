e1d1b163246050e2cbba1674bcbbd518cd2ac9d9
ddjyogi <yogendra.tanwar@gmail.com>
2016-05-26 19:23:58 +0530

    Making some improvements

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -12,7 +12,7 @@ public class Tracker {
     private List<TimeEntry> entries;
 
     public void add(TimeEntry entry) {
-        /entries.add(entry);
+        entries.add(entry);
     }
 
     public void remove(TimeEntry entry) {
@@ -24,6 +24,10 @@ public class Tracker {
     }
 
     public TimeEntry get(int index) {
+        if (valid == true){
+            //whatever
+        }
+        
         return entries.get(index);
     }
 }
